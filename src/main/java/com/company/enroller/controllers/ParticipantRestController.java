c1f5670daec0fbb7c2b5a65eb87ae3856bed0339
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2021-04-24 11:53:52 +0200

    first commit

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,4 +27,39 @@ public class ParticipantRestController {
 		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
 	}
 
+	// Get new participants
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+
+	public ResponseEntity<?> getMeeting(@PathVariable("id") String login) {
+		Participant participant = participantService.findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	}
+
+	// Post new participants
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> registerParticipant(@RequestBody Participant participant) {
+
+		if (participantService.findByLogin(participant.getLogin()) != null) {
+			return new ResponseEntity<String>("Unable to create", HttpStatus.CONFLICT);
+		}
+
+		participantService.add(participant);
+		return new ResponseEntity(HttpStatus.CREATED);
+	}
+	
+	// Delete participants
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteParticipant(@RequestBody Participant participant) {
+
+		if (participantService.findByLogin(participant.getLogin()) != null) {
+			return new ResponseEntity<String>("Unable to fin user", HttpStatus.CONFLICT);
+		}
+
+		participantService.delete(participant);
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
 }
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import javax.transaction.Transaction;
+
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -19,4 +21,20 @@ public class ParticipantService {
 		return connector.getSession().createCriteria(Participant.class).list();
 	}
 
+	public Participant findByLogin(String login) {
+		Participant participant = (Participant) connector.getSession().get(Participant.class, login);
+		return participant;
+	}
+
+	public void add(Participant participant) {
+		org.hibernate.Transaction transaction = this.connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+
+	public void delete(Participant participant) {
+		org.hibernate.Transaction transaction = this.connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();		
+	}
 }
